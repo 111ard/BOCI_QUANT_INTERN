cf59fa652881efd65fc2a7f12e8821670a814ede
11722 <1172225882@qq.com>
2023-06-01 16:41:52 +0800

    多项式

--- scalping/poly_strategy.py
+++ scalping/poly_strategy.py
@@ -5,27 +5,30 @@ from sympy import *
 from functools import partial
 import datetime as dt
 import matplotlib.pyplot as plt
+import warnings
 
+
+warnings.filterwarnings("ignore")
 rqdatac.init()
 
 
-def daily_return(test, size = 50, n = 5):
+def daily_return(test, size, n):
     x = symbols('x')
     test = test.reset_index()
-    for a in range(2,size):
-        xu = test.loc[:a, :].index
-        yu = test.loc[:a, :].open
-        reg = np.polyfit(xu, yu, n)
-        f = ''
-        for i in range(len(reg)):
-            f = f + str(reg[i]) + '*x**' + str(len(reg) - i - 1) + '+'
-        f = f[:-6]
-        expr = sympify(f)
-        f2 = lambdify(x, diff(expr, x), 'numpy')
-        test.loc[a, '一阶导数'] = f2(a)
-        f3 = lambdify(x, diff(expr, x, 2), 'numpy')
-        test.loc[a, '二阶导数'] = f3(a)
+    xu = test.loc[:size, :].index
+    yu = test.loc[:size, :].open
+    reg = np.polyfit(xu, yu, n)
+    f = ''
+    for i in range(len(reg)):
+        f = f + str(reg[i]) + '*x**' + str(len(reg) - i - 1) + '+'
+    f = f[:-6]
+    expr = sympify(f)
+    f2 = lambdify(x, diff(expr, x), 'numpy')
+    test.loc[size:size+50, '一阶导数'] = f2(np.arange(size, size+51, 1))
+    f3 = lambdify(x, diff(expr, x, 2), 'numpy')
+    test.loc[size:size+50, '二阶导数'] = f3(np.arange(size, size+51, 1))
     test['return'] = test.open / test.open.shift(1) - 1
+    test.loc[:size, '信号'] = '0'
     test.loc[(test.一阶导数 >= 0) & (test.二阶导数 >= 0), '信号'] = '多'
     test.loc[(test.一阶导数 <= 0) & (test.二阶导数 <= 0), '信号'] = '空'
     test.loc[(test.一阶导数 <= 0) & (test.二阶导数 >= 0), '信号'] = '平'
@@ -57,7 +60,7 @@ def daily_return(test, size = 50, n = 5):
         try:
             df = test.loc[start_indicator:end_indicator, 'return']
         except:
-            df = test.loc[start_indicator:start_indicator+16, 'return']
+            df = test.loc[start_indicator:start_indicator + 16, 'return']
         return np.cumprod(df + 1).iloc[-1] - 1
     if t == 1 and signal == '多':
         return np.cumprod(test.loc[start_indicator:, 'return'] + 1).iloc[-1] - 1
@@ -65,7 +68,6 @@ def daily_return(test, size = 50, n = 5):
         return np.cumprod(-test.loc[start_indicator:, 'return'] + 1).iloc[-1] - 1
     if t == 0:
         return 0
-
 
 
 #original_data = rqdatac.get_price('000905.XSHG',start_date = '20170101',end_date = '20230101',frequency = '1m')
@@ -86,17 +88,26 @@ def calculate_return(start_date,end_date,n,size,stock):
 
 # test = calculate_return(start_date = '20200101',end_date = '20210101',n =5,size =50,stock ='000905.XSHG')
 
-n_list = [2,3,4,5,6,7]
-size_list = [30,40,50,60,70,100,150,200,230]
-#n_list = [3,4]
-#size_list = [30,40]
-final_df = pd.DataFrame(columns = size_list,index = n_list)
+
+n_list = [3, 4, 5, 6, 7]
+size_list = [30, 40, 50, 100, 150, 180]
+# n_list = [3,4]
+# size_list = [30,40]
+final_df = pd.DataFrame(columns=size_list, index=n_list)
 for n in n_list:
     for size in size_list:
-        final_df.loc[n,size] = calculate_return(start_date = '20190101',end_date = '20220101',n =n,size =size,stock ='000905.XSHG')
-
+        final_df.loc[n, size] = calculate_return(start_date='20220101', end_date='20230101', n=n, size=size,
+                                                 stock='000905.XSHG')
+        print(n, size)
 for n in n_list:
-    plt.plot(final_df.columns, final_df.loc[n,:])
+    plt.plot(final_df.columns, final_df.loc[n, :])
 
 plt.legend(n_list)
 plt.show()
+
+
+
+
+
+
+
