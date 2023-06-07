ae095021ccad2713abeec7afe28d8833069a7a6e
11722 <1172225882@qq.com>
2023-06-07 23:27:28 +0800

    修改

--- alpha/factor_run.py
+++ alpha/factor_run.py
@@ -23,7 +23,7 @@ start_time = time.time()
 warnings.filterwarnings("ignore")
 rqdatac.init()
 logging.basicConfig(level=logging.DEBUG #设置日志输出格式
-                    ,filename="BOCI_QUANT_INTERN/data_process/log/demo.log" #log日志输出的文件位置和文件名
+                    ,filename="BOCI_QUANT_INTERN/alpha/log/demo.log" #log日志输出的文件位置和文件名
                     ,filemode="w" #文件的写入格式，w为重新写入文件，默认是追加
                     ,format="%(asctime)s - %(name)s - %(levelname)-9s - %(filename)-8s : %(lineno)s line - %(message)s" #日志输出的格式
                     # -8表示占位符，让输出左对齐，输出长度都为8位
