e366edfa487403c3cb76e1a82b54f5696b9a7fb3
11722 <1172225882@qq.com>
2023-06-09 10:45:20 +0800

    回测

--- alpha/factor_run.py
+++ alpha/factor_run.py
@@ -6,10 +6,11 @@ import argparse
 from rqfactor import *
 from rqfactor.notebook import *
 import multiprocessing
-from multiprocessing.pool import Pool
+import lib as lib
 import warnings
 import time
 import json
+import shutil
 
 warnings.filterwarnings('ignore')
 
@@ -18,6 +19,7 @@ class FactorRun():
     """
     优化后的类
     """
+
     def __init__(self):
         """
         初始化
@@ -47,12 +49,12 @@ class FactorRun():
     def main(self):
         if self.option == 'backtest':
             if self.category == 'selected_factors':
-                all_factors = args.selected_factors
+                all_factors = args.input
                 self.run_basic_mode(all_factors)
 
             if self.category == 'selected_folders':
                 # 扫描文件夹，找到所有的因子名，回到第一种模式
-                all_folders = args.selected_folders
+                all_folders = args.input
                 all_factors = []
                 for folder in all_folders:
                     all_factors += self.find_factors_in_folder(folder)
@@ -65,50 +67,61 @@ class FactorRun():
                 all_factors = []
                 for folder in all_folders:
                     all_factors += self.find_factors_in_folder(folder)
-                    self.run_basic_mode(all_factors)
+                self.run_basic_mode(all_factors)
 
-        if self.option =='analysis':
-            if self.category =='selected_factors':
-                all_factors = args.selected_factors
+        if self.option == 'analysis':
+            if self.category == 'selected_factors':
+                all_factors = args.input
+                self.run_analysis(all_factors)
+
+            if self.category == 'selected_folders':
+                all_folders = args.input
+                all_factors = []
+                for folder in all_folders:
+                    all_factors += self.find_factors_in_folder(folder)
+                self.run_analysis(all_factors)
+
+            if self.category == 'all':
+                # 扫描factor文件夹，得到所有的文件夹，再回到第二种模式
+                all_folders = self.scan_factor_folder()
+                all_factors = []
+                for folder in all_folders:
+                    all_factors += self.find_factors_in_folder(folder)
                 self.run_analysis(all_factors)
         #     run_analysis(all_factors)
         # if roption.report:
         #     run_report(all_factors)
 
-
     # ===================================
     # 最基础的单元是处理每个因子，抓住这个本质
-    def run_basic_mode(self,all_factors):
+    def run_basic_mode(self, all_factors):
         # 单个模式
         # for factor_name in all_factors:
         #     self.process_factor(factor_name)
 
         # !!! 在此处改成multiprocess !!!
-        if __name__ =='__main__':
-
+        if __name__ == '__main__':
             with multiprocessing.Pool(processes=4) as pool:
                 pool.map(self.process_factor, all_factors)
 
-
     # 多进程跑这个函数即可
-    def process_factor(self,factor_name):
+    def process_factor(self, factor_name):
         # 首先找到因子的分类
 
         # 1. 检查factor相关文件是否存在
         if not self.factor_exist(factor_name):
-
-            print("Factor "+factor_name+" 文件不存在")
+            print("Factor " + factor_name + " 文件不存在")
             return
 
         # 2. 检查factor的输出csv
         # 文件存在，则
         classification = self.factor_classification(factor_name)
-        self.factor_output_dirs_check(factor_name,classification)
+        self.factor_output_dirs_check(factor_name, classification)
         if self.factor_net_value_existed(factor_name, classification):
 
             existed_nv_df = pd.read_csv(
                 os.path.join(self.pwd, 'result', classification, factor_name, 'csv', 'factor_value',
-                             factor_name + '_factor_value.csv'),index_col = 0)
+                             factor_name + '_factor_value.csv'), index_col=0)
             backtest_started_date = existed_nv_df.index[-1]
             # 如果已经是最新
             # print("".join(backtest_started_date.split('-'))[:8],'|||',self.end_date)
@@ -120,54 +133,57 @@ class FactorRun():
             factor_df = existed_nv_df.append(
                 self.run_factor(factor_name, classification, backtest_started_date, self.end_date).iloc[1:])
 
-
         # 文件不存在，则从头开始读
         if not self.factor_net_value_existed(factor_name, classification):
-            print('因子'+factor_name+'不存在，从头开始计算')
-            factor_df = self.run_factor(factor_name,classification,self.start_date,self.end_date)
+            print('因子' + factor_name + '不存在，从头开始计算')
+            factor_df = self.run_factor(factor_name, classification, self.start_date, self.end_date)
 
-        factor_df.to_csv(os.path.join(self.pwd,'result',classification,factor_name,'csv','factor_value',factor_name+'_factor_value.csv'))
-        print('因子'+factor_name+'已经计算完毕并输出csv文件')
+        factor_df.to_csv(os.path.join(self.pwd, 'result', classification, factor_name, 'csv', 'factor_value',
+                                      factor_name + '_factor_value.csv'))
+        print('因子' + factor_name + '已经计算完毕并输出csv文件')
 
-
-    def factor_exist(self,factor_name):
-        factor_py_name = factor_name+ '.py'
-        for root, dirs, files in os.walk(os.path.join(self.pwd,'factor')):
+    def factor_exist(self, factor_name):
+        factor_py_name = factor_name + '.py'
+        for root, dirs, files in os.walk(os.path.join(self.pwd, 'factor')):
 
             if factor_py_name in files:
                 return 1
         return 0
 
-    def factor_net_value_existed(self,factor_name,classification):
+    def factor_net_value_existed(self, factor_name, classification):
 
-
-        path  = os.path.join(self.pwd,'result',classification,factor_name,'csv','factor_value')
+        path = os.path.join(self.pwd, 'result', classification, factor_name, 'csv', 'factor_value')
         if factor_name + '_factor_value.csv' not in os.listdir(path):
             return 0
         return 1
 
-
-    def run_factor(self,factor_name,classification,start_date,end_date):
+    def run_factor(self, factor_name, classification, start_date, end_date):
         # 跑因子值，存成文件
-        factor_full_path_module = 'factor.'+classification+'.'+factor_name
+        factor_full_path_module = 'factor.' + classification + '.' + factor_name
         module = importlib.import_module(factor_full_path_module)
 
         result = getattr(module, 'main')()
-        return execute_factor(result,self.stock_pool,start_date,end_date)
+        try:
+            df = execute_factor(result, self.stock_pool, start_date, end_date)
+            return df
+        except TypeError:
+            print('因子' + factor_name + '返回值为none，若是子定义因子请检查定义，若是调用米筐内置因子请检查是否存在此因子')
+            with open(os.path.join(self.pwd, "test.txt"), "w") as f:
+                f.write(factor_name + '  , ')
 
-    def factor_classification(self,factor_name):
+    def factor_classification(self, factor_name):
         factor_py_name = factor_name + '.py'
-        for root, dirs, files in os.walk(os.path.join(self.pwd,'factor')):
+        for root, dirs, files in os.walk(os.path.join(self.pwd, 'factor')):
             if factor_py_name in files:
                 return os.path.basename(root)
 
-    def find_factors_in_folder(self,folder):
-        factor_temp_list=[]
-        folder_path= os.path.join(self.pwd, 'factor',folder)
-        if folder not in os.listdir(os.path.join(self.pwd,'factor/')):
-            print(folder+'文件夹不存在')
+    def find_factors_in_folder(self, folder):
+        factor_temp_list = []
+        folder_path = os.path.join(self.pwd, 'factor', folder)
+        if folder not in os.listdir(os.path.join(self.pwd, 'factor/')):
+            print(folder + '文件夹不存在')
         else:
-            print(folder+'文件夹已经找到，开始扫描')
+            print(folder + '文件夹已经找到，开始扫描')
             for root, dirs, files in os.walk(folder_path):
                 for file in files:
                     if file.endswith('.py'):
@@ -175,42 +191,65 @@ class FactorRun():
         return factor_temp_list
 
     def scan_factor_folder(self):
-        return os.listdir(os.path.join(self.pwd,'factor'))
-    
-    def factor_output_dirs_check(self,factor_name,classification):
-        
+        return os.listdir(os.path.join(self.pwd, 'factor'))
 
-        os.makedirs(self.pwd +'/result/' + classification, exist_ok=True)
-        os.makedirs(self.pwd +'/result/' + classification+ '/'+factor_name + '/jpg', exist_ok=True)
-        os.makedirs(self.pwd +'/result/'  + classification+ '/'+factor_name + '/csv', exist_ok=True)
-        os.makedirs(self.pwd +'/result/'  + classification+'/'+ factor_name + '/csv/'+ 'ic_analysis', exist_ok=True)
-        os.makedirs(self.pwd +'/result/'  + classification+ '/'+factor_name + '/csv/'+ 'quantile_analysis', exist_ok=True)
-        os.makedirs(self.pwd +'/result/'  + classification+ '/'+factor_name + '/csv/'+ 'return_analysis', exist_ok=True)
-        os.makedirs(self.pwd +'/result/'  + classification+ '/'+factor_name + '/csv/'+ 'factor_value', exist_ok=True)        # 此处dropna会去掉1、ST的股票以及2、米筐中不存在其数据的股票
+    def factor_output_dirs_check(self, factor_name, classification):
 
-    def run_analysis(self,factors):
+        os.makedirs(self.pwd + '/result/' + classification, exist_ok=True)
+        os.makedirs(self.pwd + '/result/' + classification + '/' + factor_name + '/jpg', exist_ok=True)
+        os.makedirs(self.pwd + '/result/' + classification + '/' + factor_name + '/csv', exist_ok=True)
+        os.makedirs(self.pwd + '/result/' + classification + '/' + factor_name + '/csv/' + 'ic_analysis', exist_ok=True)
+        os.makedirs(self.pwd + '/result/' + classification + '/' + factor_name + '/csv/' + 'quantile_analysis',
+                    exist_ok=True)
+        os.makedirs(self.pwd + '/result/' + classification + '/' + factor_name + '/csv/' + 'return_analysis',
+                    exist_ok=True)
+        os.makedirs(self.pwd + '/result/' + classification + '/' + factor_name + '/csv/' + 'factor_value',
+                    exist_ok=True)  # 此处dropna会去掉1、ST的股票以及2、米筐中不存在其数据的股票
+
+    def run_analysis(self, factors):
         """
-        用于对一个银子列表的分析
+        分析因子列表
         :param factors:
         :return:
         """
+        try:
+            shutil.rmtree(os.path.join(self.pwd, 'result', 'temp_dir'))
+        except:
+            pass
+        os.makedirs(os.path.join(self.pwd, 'result', 'temp_dir'))
         for factor in factors:
             self.unit_factor_analysis(factor)
+        lib.OutputResult(factor_result_path = '', pwd = self.pwd).generate_report()
 
+    def unit_factor_analysis(self, factor):
+        file_name = factor + '_factor_value.csv'
+        classifcation = self.factor_classification(factor)
+        file_path = os.path.join(self.pwd, 'result', classifcation, factor)
+        csv_path = os.path.join(self.pwd, 'result', classifcation, factor, 'csv', 'factor_value')
+        if file_name not in os.listdir(csv_path):
+            print('因子'+factor+'净值文件不存在')
+            return
+        else:
+            print('因子'+factor+'净值文件已找到并读取，开始分析')
+            df = pd.read_csv(os.path.join(csv_path, factor + '_factor_value.csv'), index_col=0)
+            df.index = pd.to_datetime(df.index)
+            lib.OutputResult(file_path, self.pwd).factor_analysis(df, factor)
+            print('因子'+factor+'已经完成分析')
 
 
 start_time = time.time()
+print('程序开始执行')
 rqdatac.init()
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--category', type=str, help='execute_category')
 parser.add_argument('--option', type=str, help='execute_option')
-parser.add_argument('--selected_factors', type=str, nargs='+', help='factors_input')
-parser.add_argument('--selected_folders', type=str, nargs='+', help='folders_input')
+parser.add_argument('--input', type=str, nargs='+', help='factors_or_folders_input')
+
 
 args = parser.parse_args()
 
 FactorRun().main()
 end_time = time.time()
-
+print('程序执行完毕')
 print('用时为' + str(end_time - start_time) + 's')
---
+++ alpha/lib.py
@@ -0,0 +1,80 @@
+from rqfactor import *
+from rqfactor.notebook import *
+import rqdatac
+import os
+import shutil
+import pandas as pd
+import numpy as np
+
+
+class OutputResult():
+    """
+    用于使用ricequant函数输出因子的IC、分组回测表现以及收益情况
+    """
+    def __init__(self,factor_result_path,pwd):
+        """
+        初始化
+        """
+        self.factor_result_path = factor_result_path
+        self.pwd = pwd
+
+    def factor_analysis(self, df,factor_name):
+        """
+        用于输出一个因子的回测结果，放入文件夹中
+        :param df: 因子值
+        """
+
+        engine = FactorAnalysisEngine()
+        engine.append(('rank_ic_analysis', ICAnalysis(rank_ic=True, industry_classification='sws', max_decay=3)))
+        engine.append(('quantile', QuantileReturnAnalysis(quantile=5, benchmark=None)))
+        engine.append(('return', FactorReturnAnalysis()))
+        result = engine.analysis(df, 'daily', ascending=True, periods=[1], keep_preprocess_result=False)
+        result['rank_ic_analysis'].summary().to_csv(self.factor_result_path + '/csv/'+ 'ic_analysis/'+"rank_ic_analysis.csv")
+        result['rank_ic_analysis'].ic.to_csv(self.factor_result_path + '/csv/'+ 'ic_analysis/'+'ic_analysis.csv')
+        result['rank_ic_analysis'].ic_decay.to_csv(self.factor_result_path + '/csv/'+ 'ic_analysis/'+'ic_decay.csv')
+        result['rank_ic_analysis'].ic_industry_distribute.to_excel(self.factor_result_path + '/csv/'+ 'ic_analysis/'+'ic_industry_distribute.xlsx')
+        result['quantile'].quantile_returns.to_csv(self.factor_result_path + '/csv/'+ 'quantile_analysis/'+'quantile_returns.csv')
+        result['quantile'].quantile_turnover.to_csv(self.factor_result_path + '/csv/'+ 'quantile_analysis/'+'quantile_turnover.csv')
+        result['quantile'].top_minus_bottom_returns.to_csv(self.factor_result_path + '/csv/'+ 'quantile_analysis/'+'top_minus_bottom_returns.csv')
+        result['quantile'].quantile_detail.to_csv(self.factor_result_path + '/csv/'+ 'quantile_analysis/'+'quantile_detail.csv')
+        result['return'].factor_returns.to_csv(self.factor_result_path + '/csv/'+ 'return_analysis/'+'factor_returns.csv')
+        result['return'].max_drawdown().to_csv(self.factor_result_path + '/csv/'+ 'return_analysis/'+'factor_returns_max_drawdown.csv')
+        result['return'].std().to_csv(self.factor_result_path + '/csv/'+ 'return_analysis/'+'factor_returns_std.csv')
+
+        for k in (['rank_ic_analysis', 'quantile', 'return']):
+            try:
+                result[k].show()
+                shutil.move(k + '.html', self.factor_result_path +'/jpg')
+
+            except:
+                pass
+
+            try:
+                os.remove(k + '.html')
+            except:
+                pass
+        top_minus_bottom_returns = float(result['quantile'].top_minus_bottom_returns['P_1'] [-1])
+        factor_return = result['return'].factor_returns['P_1'][-1]
+        factor_mdd = result['return'].max_drawdown().iloc[0]
+        factor_std = result['return'].std().iloc[0]
+        temp_dataframe = pd.DataFrame(
+            {'factor_name': [top_minus_bottom_returns, factor_return, factor_mdd, factor_std]},
+            index=['第一组减第五组累积收益', '因子累积收益(米筐算法)', '因子收益最大回撤', '因子收益波动率'])
+        factor_average_ic_df = pd.DataFrame(result['rank_ic_analysis'].summary()['P_1'])
+        factor_average_ic_df.index = 'IC_' + factor_average_ic_df.index
+        factor_average_ic_df.columns = ['factor_name']
+        final = temp_dataframe.append(factor_average_ic_df)
+        final.to_csv(os.path.join(self.pwd, 'result', 'temp_dir', factor_name+'_temp.csv'))
+
+    def generate_report(self):
+        df = []
+        temp_path = self.pwd + '/result/temp_dir'
+        temp_list = os.listdir(temp_path)
+        for report_unique in temp_list:
+            temp = pd.read_csv(os.path.join(temp_path, report_unique)).T
+            df.append(temp)
+        df = pd.concat(df).T
+        df = df.T.drop_duplicates()
+        df = df.T.set_index('Unnamed: 0', drop=True)
+        df.T.to_excel(self.pwd + '/result/summary.xlsx')
+
