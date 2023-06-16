da9336cf2c68bfc4c54a8be67e460256463edfd4
11722 <1172225882@qq.com>
2023-06-16 09:49:13 +0800

    修改

--- alpha/factor_run.py
+++ alpha/factor_run.py
@@ -213,7 +213,7 @@ class FactorRun():
         except (TypeError ,AttributeError):
             logging.info('因子' + factor_name + '返回值为none，若是自定义因子请检查定义，若是调用米筐内置因子请检查是否存在此因子')
             #print('因子' + factor_name + '返回值为none，若是子定义因子请检查定义，若是调用米筐内置因子请检查是否存在此因子')
-            with open(os.path.join(self.pwd, "test.txt"), "w") as f:
+            with open(os.path.join(self.pwd, "omitted.txt"), "w") as f:
                 f.write(factor_name + '  , ')
 
     def factor_classification(self, factor_name):
